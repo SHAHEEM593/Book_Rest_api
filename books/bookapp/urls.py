6b4d899d33e1d13927cf8980783edb53d9ea1b63
SHAHEEM593 <122553651+SHAHEEM593@users.noreply.github.com>
2023-07-05 16:59:00 +0530

    urls.py

--- books/bookapp/urls.py
+++ books/bookapp/urls.py
@@ -3,5 +3,5 @@ from .views import BookListCreateView, BookRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('books/', BookListCreateView.as_view(), name='book-list-create'),
-    path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='book-retrieve-update-destroy'),
+    path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='book-update'),
 ]
